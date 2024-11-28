ef48f5c5b2d95faaf53a59907a225725259fe9da
Aadarsh Desai <adidesai91@gmail.com>
2024-11-28 01:50:24 -0500

    Updated Herosection

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Navbar } from "./components/navbar";
-import { Herosection } from "./components/herosection";
 import { FeatureSection } from "./components/FeatureSection";
 import { WorkflowSection } from "./components/WorkflowSection";
 import { Price } from "./components/Price";
 import { Testimonials } from "./components/Testimonials";
 import { Footer } from "./components/Footer";
+import { Herosection } from "./components/herosection";
 function App() {
 
   return (
